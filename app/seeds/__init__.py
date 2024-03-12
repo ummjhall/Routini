db9ae84660b10e5f4dc201249e4eae43295d126b
Justin Hall <justinhall.jnh@gmail.com>
2024-03-13 03:42:13 +0900

    Add seed commands

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,7 +1,13 @@
 from flask.cli import AppGroup
-from .users import seed_users, undo_users
-
 from app.models.db import db, environment, SCHEMA
+from .users import seed_users, undo_users
+from .avatars import seed_avatars, undo_avatars
+from .tasks import seed_tasks, undo_tasks
+from .rewards import seed_rewards, undo_rewards
+from .equipment import seed_equipment, undo_equipment
+from .avatars_equipment import seed_avatars_equipment, undo_avatars_equipment
+from .images import seed_images, undo_images
+
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -15,10 +21,22 @@ def seed():
         # Before seeding in production, you want to run the seed undo
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
+        # Make sure to add all your other models' undo functions below
+        undo_images()
+        undo_avatars_equipment()
+        undo_equipment()
+        undo_rewards()
+        undo_tasks()
+        undo_avatars()
         undo_users()
+
     seed_users()
-    # Add other seed functions here
+    seed_avatars()
+    seed_tasks()
+    seed_rewards()
+    seed_equipment()
+    seed_avatars_equipment()
+    seed_images()
 
 
 # Creates the `flask seed undo` command
