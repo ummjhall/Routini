f0eeced0a338d9422bab0a2f721aa93c0e57f505
Justin Hall <justinhall.jnh@gmail.com>
2024-03-14 13:48:53 +0900

    Rework api endpoint: Get all of the Current User's Equipment

--- app/api/inventory_routes.py
+++ app/api/inventory_routes.py
@@ -1,50 +1,26 @@
 from flask import Blueprint
 from app.models import Avatar, AvatarEquipment
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 inventory_routes = Blueprint("inventory", __name__, url_prefix="/api/equipment")
 
 
 @inventory_routes.route("/current")
-def get_users_equipment():
-    if current_user:
-        formattedJson = {"Equipment": []}
-        userAvatar = current_user.avatar
+@login_required
+def get_user_equipment():
+    avatar = current_user.avatar
+    if not avatar:
+        return {'Equipment': []}
 
-        if userAvatar:
-            equipments = userAvatar.equipment
-            for equipment in equipments:
-                equipment_image = equipment.image.to_dict()["url"]
+    owned_equipment = []
+    for owned_item in avatar.equipment:
+        item = owned_item.to_dict()
 
-                all_equipment = equipment.to_dict()
-                avatarEquip = AvatarEquipment.query.filter(
-                    AvatarEquipment.equipment_id == equipment.id
-                ).first()
-                equipment_nickname = avatarEquip.equipment_nickname
-                all_equipment["nickname"] = equipment_nickname
-                all_equipment["image_url"] = equipment_image
-                all_equipment["user_id"] = current_user.id
-                formattedJson["Equipment"].append(all_equipment)
+        item['user_id'] = current_user.id
+        item['imgae_url'] = owned_item.image.to_dict()['url']
+        item['nickname'] = AvatarEquipment.query.filter(
+                AvatarEquipment.equipment_id == owned_item.id).first().equipment_nickname
 
-    else:
-        print("Login!")
+        owned_equipment.append(item)
 
-    return formattedJson
-
-
-# {
-#   "Equipment": [
-#     {
-#       "id": 1,
-#       "user_id": 1,
-#       "type": "main",
-#       "name": "Sword of Destiny",
-#       "nickname": "Slicer",
-#       "description": "The best sword ever.",
-#       "cost": 100,
-#       "image_url": "image url",
-#       "created_at": "2021-11-19 20:39:36",
-#       "updated_at": "2021-11-19 20:39:36"
-#     }
-#   ]
-# }
+    return {'Equipment': owned_equipment}
