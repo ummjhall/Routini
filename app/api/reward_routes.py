bdf71a2b90d7fbf4e855794a1dd6e9a037a1ab78
Ramon <ramonpbarros@gmail.com>
2024-03-16 14:56:44 -0700

    body validations are now implemented, new reward form created

--- app/api/reward_routes.py
+++ app/api/reward_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from app.models import Reward, db
 from flask_login import current_user, login_required
+from app.forms import RewardForm
 
 
 reward_routes = Blueprint("rewards", __name__, url_prefix="/api/rewards")
@@ -22,23 +23,48 @@ def get_user_rewards():
 @reward_routes.route("/current", methods=["POST"])
 @login_required
 def create_reward():
+    form = RewardForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
     reward_data = request.json
 
-    if not reward_data:
-        return {"message": "Bad Request"}, 400
+    errors = {}
 
-    new_reward = Reward(
-        user_id=current_user.id,
-        type=reward_data.get("type", "custom"),
-        title=reward_data.get("title"),
-        description=reward_data.get("description"),
-        cost=reward_data.get("cost", 0),
-    )
+    if form.validate_on_submit():
+        new_reward = Reward(
+            user_id=current_user.id,
+            type=reward_data.get("type", "custom"),
+            title=reward_data.get("title"),
+            description=reward_data.get("description"),
+            cost=reward_data.get("cost", 0),
+        )
 
-    db.session.add(new_reward)
-    db.session.commit()
+        db.session.add(new_reward)
+        db.session.commit()
 
-    return new_reward.to_dict(), 201
+        return new_reward.to_dict(), 201
+    else:
+        for field, error_msgs in form.errors.items():
+            for msg in error_msgs:
+                if field not in errors:
+                    errors[field] = []
+                errors[field].append(msg)
+
+    if "type" in form.errors and "This field is required." in form.errors["type"]:
+        errors["type"] = "Type is required"
+
+    if "title" in form.errors and "This field is required." in form.errors["title"]:
+        errors["title"] = "Title is required"
+
+    if (
+        "description" in form.errors
+        and "This field is required." in form.errors["description"]
+    ):
+        errors["description"] = "description is required"
+    if "cost" in form.errors and "This field is required." in form.errors["cost"]:
+        errors["cost"] = "cost is required"
+
+    return {"message": "Bad request", "errors": errors}, 400
 
 
 @reward_routes.route("/<reward_id>", methods=["PUT"])
