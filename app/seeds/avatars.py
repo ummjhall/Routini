5aa3ad5a3604a908beefcbee74f57eb68a823d85
Justin Hall <justinhall.jnh@gmail.com>
2024-03-17 01:24:08 +0900

    Backend > User Model: Delete first and last name columns

--- app/seeds/avatars.py
+++ app/seeds/avatars.py
@@ -5,7 +5,6 @@ from sqlalchemy.sql import text
 def seed_avatars():
     seeds = [
         Avatar(user_id=1, name='demo_av_name', bio='char bio', level=0, health=50, exp=0, gold=0, gems=0),
-        Avatar(user_id=2, name='ramon_av_name', bio='char bio', level=0, health=50, exp=0, gold=0, gems=0),
         Avatar(user_id=3, name='nikola_av_name', bio='char bio', level=0, health=50, exp=0, gold=0, gems=0),
         Avatar(user_id=4, name='justin_av_name', bio='char bio', level=0, health=50, exp=0, gold=0, gems=0)
     ]
