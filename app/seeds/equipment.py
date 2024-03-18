51384ab52af371f368333e427e4a5ed4b567556b
Ramon <ramonpbarros@gmail.com>
2024-03-17 21:26:55 -0700

    backend > seeds > equipment: updated string values

--- app/seeds/equipment.py
+++ app/seeds/equipment.py
@@ -4,9 +4,9 @@ from sqlalchemy.sql import text
 
 def seed_equipment():
     seeds = [
-        Equipment(type='head', name='helmet name', description='helmet description', cost=100),
-        Equipment(type='main', name='weapon name', description='weapon description', cost=100),
-        Equipment(type='armor', name='armor name', description='armor description', cost=100),
+        Equipment(type='head', name='Helmet of Protection', description='A sturdy helmet imbued with protective enchantments.', cost=100),
+        Equipment(type='main', name='Sword of Fire', description='A powerful sword that bursts into flames upon striking.', cost=100),
+        Equipment(type='armor', name='Dragonhide Armor', description='Armor crafted from the scales of a mighty dragon, offering exceptional defense.', cost=100),
     ]
 
     for equipment in seeds:
