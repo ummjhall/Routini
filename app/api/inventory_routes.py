08bb014059e096d6d7825a5c475427e91c2c9d7f
Justin Hall <justinhall.jnh@gmail.com>
2024-03-18 18:30:09 +0900

    Backend: DRY up Equipment routes

--- app/api/inventory_routes.py
+++ app/api/inventory_routes.py
@@ -12,6 +12,10 @@ def get_shop_equipment():
     Get all Equipment available for purchase for the Current User
     """
 
+    # User does not currently have an Avatar
+    if not current_user.avatar:
+        return {'message': "Avatar couldn't be found"}, 404
+
     all_equipment = Equipment.query.all()
 
     shop_equipment = []
@@ -30,28 +34,30 @@ def get_user_equipment():
     Get all of the Current User's Equipment
     """
 
-    avatar = current_user.avatar
-    if not avatar:
-        return {'Equipment': []}
+    # User does not currently have an Avatar
+    if not current_user.avatar:
+        return {'message': "Avatar couldn't be found"}, 404
 
     owned_equipment = []
-    for owned_item in avatar.equipment:
+    for owned_item in current_user.avatar.equipment:
         item = owned_item.to_dict()
         item['user_id'] = current_user.id
         item['image_url'] = owned_item.image.to_dict()['url']
         item['nickname'] = AvatarEquipment.query.filter(
                 AvatarEquipment.equipment_id == owned_item.id and
-                AvatarEquipment.avatar_id == avatar.id).one().equipment_nickname
+                AvatarEquipment.avatar_id == current_user.avatar.id).one().equipment_nickname
         owned_equipment.append(item)
 
     return {'Equipment': owned_equipment}
 
 
-@inventory_routes.route('/current/<equipment_id>', methods=['POST'])
+@inventory_routes.route('/current/<equipment_id>', methods=['POST', 'PUT', 'PATCH', 'DELETE'])
 @login_required
-def collect_equipment(equipment_id):
+def handle_equipment(equipment_id):
     """
-    Buy or collect a piece of Equipment for the Current User
+    POST: Buy or collect a piece of Equipment for the Current User
+    PUT/PATCH: Rename a piece of Equipment belonging to the Current User
+    DELETE: Remove a piece of Equipment from the Current User's inventory
     """
 
     # User does not currently have an Avatar
@@ -63,85 +69,42 @@ def collect_equipment(equipment_id):
     if not found:
         return {'message': "Equipment couldn't be found"}, 404
 
-    # User already owns specified Equipment
+    # Check if user owns specified equipment
     avatar_id = current_user.avatar.id
     owned = AvatarEquipment.query.filter(
         AvatarEquipment.equipment_id == equipment_id and AvatarEquipment.avatar_id == avatar_id
         ).one_or_none()
-    if owned:
+    if owned and request.method == 'POST':
         return {'message': 'Equipment already owned'}, 400
-
-    # SUCCESS
-    new_equipment = AvatarEquipment(
-        avatar_id=current_user.avatar.to_dict()['id'],
-        equipment_id=equipment_id
-    )
-    db.session.add(new_equipment)
-    db.session.commit()
-
-    return new_equipment.to_dict(), 201
-
-
-@inventory_routes.route('/current/<equipment_id>', methods=['PUT', 'PATCH'])
-@login_required
-def rename_equipment(equipment_id):
-    """
-    Renames the user's piece of Equipment specified by id and returns it.
-    """
-
-    # User does not currently have an Avatar
-    if not current_user.avatar:
-        return {'message': "Avatar couldn't be found"}, 404
-
-    # Couldn't find Equipment with the specified id
-    found = Equipment.query.get(equipment_id)
-    if not found:
-        return {'message': "Equipment couldn't be found"}, 404
-
-    # User doesn't own specified Equipment
-    avatar_id = current_user.avatar.id
-    owned = AvatarEquipment.query.filter(
-        AvatarEquipment.equipment_id == equipment_id and AvatarEquipment.avatar_id == avatar_id
-        ).one_or_none()
-    if not owned:
+    if not owned and request.method in ['PUT', 'PATCH', 'DELETE']:
         return {'message': 'Equipment unowned'}, 400
 
-    # SUCCESS
-    nickname = request.json.get('nickname', None)
-    if nickname:
-        owned.equipment_nickname = nickname
-        db.session.add(owned)
+    # POST: BUY OR COLLECT A PIECE OF EQUIPMENT
+    if request.method == 'POST':
+        new_equipment = AvatarEquipment(
+            avatar_id=current_user.avatar.to_dict()['id'],
+            equipment_id=equipment_id
+        )
+        db.session.add(new_equipment)
         db.session.commit()
 
-    return owned.to_dict()
+        return new_equipment.to_dict(), 201
 
+    # PUT/PATCH: RENAME A PIECE OF EQUIPMENT
+    if request.method in ['PUT', 'PATCH']:
+        nickname = request.json.get('nickname', None)
+        if nickname:
+            owned.equipment_nickname = nickname
+            db.session.add(owned)
+            db.session.commit()
 
-@inventory_routes.route('/current/<equipment_id>', methods=['DELETE'])
-@login_required
-def delete_owned_equipment(equipment_id):
-    """
-    Deletes a piece of owned Equipment from the user's inventory.
-    """
+        return owned.to_dict()
 
-    # User does not currently have an Avatar
-    if not current_user.avatar:
-        return {'message': "Avatar couldn't be found"}, 404
+    # DELETE: REMOVE A PIECE OF EQUIPMENT FROM INVENTORY
+    if request.method == 'DELETE':
+        db.session.delete(owned)
+        db.session.commit()
 
-    # Couldn't find Equipment with the specified id
-    found = Equipment.query.get(equipment_id)
-    if not found:
-        return {'message': "Equipment couldn't be found"}, 404
+        return {'message': 'Successfully deleted'}
 
-    # User doesn't own specified Equipment
-    avatar_id = current_user.avatar.id
-    owned = AvatarEquipment.query.filter(
-        AvatarEquipment.equipment_id == equipment_id and AvatarEquipment.avatar_id == avatar_id
-        ).one_or_none()
-    if not owned:
-        return {'message': 'Equipment unowned'}, 400
-
-    # SUCCESS
-    db.session.delete(owned)
-    db.session.commit()
-
-    return {'message': 'Successfully deleted'}
+    return {'message': 'Internal server error'}, 500
