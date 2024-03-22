a97e99b76a52d676343de6972d84a8f0568b1745
Nikola Milinovich <nmilinovich1@gmail.com>
2024-03-21 23:22:48 -0700

    fixed merge comma missing

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -12,7 +12,7 @@ import taskReducer from "./tasks";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-  tasks: taskReducer
+  tasks: taskReducer,
   equipment: equipmentReducer
 });
 
--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -10,7 +10,6 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        tasksfrontend
         path: "/",
         element: <TaskLandingPage />,
       },
