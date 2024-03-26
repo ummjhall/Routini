519be378bdf26a3a9f232b3a4723fff459feec64
Justin Hall <justinhall.jnh@gmail.com>
2024-03-26 13:04:45 +0900

    Frontend > Nav: Debug logout from Shop page

--- react-vite/src/components/Shop/Shop.jsx
+++ react-vite/src/components/Shop/Shop.jsx
@@ -1,6 +1,11 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 import ViewAvatar from "../ViewAvatar/ViewAvatar";
 
 function Shop() {
+  const user = useSelector(state => state.session.user);
+
+  if (!user) return <Navigate to='/signup' replace={true} />;
 
   return (
     <>
