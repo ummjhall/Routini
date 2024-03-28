2308e727e2eaf870338b70905bacf491168debc3
Justin Hall <justinhall.jnh@gmail.com>
2024-03-29 01:25:18 +0900

    Frontend: Update various css

--- react-vite/src/components/ViewAvatar/ViewAvatar.jsx
+++ react-vite/src/components/ViewAvatar/ViewAvatar.jsx
@@ -33,68 +33,70 @@ function ViewAvatar() {
   return (
     <>
       <div className="view-avatar-banner">
-        <div className="tasks-container">
-          <div className="frame">
-            <div className="overlay">
-              <div className="avatar" onClick={handleImgClick}>
-                <img
-                  src={userAvatar?.image_url}
-                  alt="avatar-image"
-                  style={{ display: userAvatar ? 'block' : 'none' }}
-                />
+        <div className="view-avatar-container">
+          <div className='view-avatar-sub-container'>
+            <div className="frame">
+              <div className="overlay">
+                <div className="avatar" onClick={handleImgClick}>
+                  <img
+                    src={userAvatar?.image_url}
+                    alt="avatar-image"
+                    style={{ display: userAvatar ? 'block' : 'none' }}
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="stats">
-            <div className="name">
-              <img
-                className="name-img"
-                src="https://res.cloudinary.com/drv1e8rjp/image/upload/v1711252236/name-icon_f0g6li.png"
-                alt="name-icon"
-              />
-              <div className="name-content">
-                <p>{userAvatar?.name}</p>
-                <div className="name-small">
-                  <small>{user?.username} • </small>
-                  <small>Level {userAvatar?.level} Warrior</small>
+            <div className="stats">
+              <div className="name">
+                <img
+                  className="name-img"
+                  src="https://res.cloudinary.com/drv1e8rjp/image/upload/v1711252236/name-icon_f0g6li.png"
+                  alt="name-icon"
+                />
+                <div className="name-content">
+                  <p>{userAvatar?.name}</p>
+                  <div className="name-small">
+                    <small>{user?.username} • </small>
+                    <small>Level {userAvatar?.level} Warrior</small>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="health">
-              <img
-                className="health-img"
-                src="https://res.cloudinary.com/drv1e8rjp/image/upload/v1711252844/health-icon_ppszq6.png"
-                alt="health-icon"
-              />
-              <div className="health-content">
-                <div className="health-bar-border">
-                  <div
-                    className="health-bar"
-                    style={{
-                      width: `${(userAvatar?.health || 0) * 4}px`,
-                    }}
-                  ></div>
+              <div className="health">
+                <img
+                  className="health-img"
+                  src="https://res.cloudinary.com/drv1e8rjp/image/upload/v1711252844/health-icon_ppszq6.png"
+                  alt="health-icon"
+                />
+                <div className="health-content">
+                  <div className="health-bar-border">
+                    <div
+                      className="health-bar"
+                      style={{
+                        width: `${(userAvatar?.health || 0) * 4}px`,
+                      }}
+                    ></div>
+                  </div>
+                  <small>{userAvatar?.health} / 50</small>
                 </div>
-                <small>{userAvatar?.health} / 50</small>
               </div>
-            </div>
-            <div className="exp">
-              <img
-                className="exp-img"
-                src="https://res.cloudinary.com/drv1e8rjp/image/upload/v1711252236/exp-icon_uu1j9z.png"
-                alt="exp-icon"
-              />
-              <div className="exp-content">
-                <div className="exp-bar-border">
-                  <div
-                    className="exp-bar"
-                    style={{
-                      width: `${(userAvatar?.exp || 0) * 2.666}px`,
-                    }}
-                  ></div>
+              <div className="exp">
+                <img
+                  className="exp-img"
+                  src="https://res.cloudinary.com/drv1e8rjp/image/upload/v1711252236/exp-icon_uu1j9z.png"
+                  alt="exp-icon"
+                />
+                <div className="exp-content">
+                  <div className="exp-bar-border">
+                    <div
+                      className="exp-bar"
+                      style={{
+                        width: `${(userAvatar?.exp || 0) * 2.666}px`,
+                      }}
+                    ></div>
+                  </div>
+                  <small>{userAvatar?.exp} / 75</small>
                 </div>
-                <small>{userAvatar?.exp} / 75</small>
               </div>
             </div>
           </div>
