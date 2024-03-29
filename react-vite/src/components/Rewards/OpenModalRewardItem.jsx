322a2b34468eddb0f7997c3fbdafc04fafa98a17
Nikola Milinovich <nmilinovich1@gmail.com>
2024-03-28 21:28:23 -0700

    reward use removes gold from avatar

--- react-vite/src/components/Rewards/OpenModalRewardItem.jsx
+++ react-vite/src/components/Rewards/OpenModalRewardItem.jsx
@@ -1,12 +1,13 @@
 import { useModal } from '../../context/Modal';
 import './RewardItemTile.css';
 
-function OpenModalMenuItem({
+function OpenModalRewardItem({
   modalComponent, // component to render inside the modal
   itemText, // text of the button that opens the modal
   onItemClick, // optional: callback function that will be called once the button that opens the modal is clicked
   onModalClose, // optional: callback function that will be called once the modal is closed
   reward,
+  avatar,
 }) {
   const { setModalContent, setOnModalClose } = useModal();
 
@@ -15,6 +16,15 @@ function OpenModalMenuItem({
     setModalContent(modalComponent);
     if (typeof onItemClick === 'function') onItemClick();
   };
+  const useReward = async (e) => {
+    e.preventDefault();
+    const updatedAvatar = {
+        gold: avatar.gold - reward.cost,
+    };
+
+    return dispatch(editUserAvatar(updatedAvatar))
+    .then(alert(`${reward.cost} gold was taken from your avatar to use your ${reward.title} reward`))
+  }
 
   return (
     <div className="reward-tile-content" onClick={onClick}>
@@ -22,7 +32,7 @@ function OpenModalMenuItem({
         <p>{itemText}</p>
         <small>{reward.description}</small>
       </div>
-      <div onClick={() => {alert('works')}} className="reward-cost">
+      <div onClick={useReward} className="reward-cost">
         <img
           className="reward-coin-img"
           src="https://res.cloudinary.com/drv1e8rjp/image/upload/v1711434244/coin_l2gdi1.png"
@@ -34,4 +44,4 @@ function OpenModalMenuItem({
   );
 }
 
-export default OpenModalMenuItem;
+export default OpenModalRewardItem;
--- react-vite/src/components/Rewards/RewardItemTile.jsx
+++ react-vite/src/components/Rewards/RewardItemTile.jsx
@@ -1,16 +1,17 @@
-import OpenModalRewardItem from '../Rewards/OpenModalRewardItem';
+import OpenModalMenuItem from '../Navigation/OpenModalMenuItem.jsx'
 import EditRewardModal from '../EditRewardModal/EditRewardModal';
+import UseRewardItem from './UseRewardItem.jsx'
 import './RewardItemTile.css';
-
-function RewardItemTile({ user, reward }) {
+function RewardItemTile({ user, reward, avatar }) {
   return (
     <>
       <div className="reward-tile">
-        <OpenModalRewardItem
-          reward={reward}
+        <OpenModalMenuItem
           itemText={reward.title}
-          modalComponent={<EditRewardModal user={user} reward={reward} />}
+          modalComponent={<EditRewardModal user={user} reward={reward} avatar={avatar} />}
+          customClass={'reward-button-for-modal'}
         />
+        <UseRewardItem user={user} reward={reward} avatar={avatar} />
       </div>
     </>
   );
