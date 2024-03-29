a178bbeb1d57661b94a1b220a2f44cec40ba41be
Ramon <ramonpbarros@gmail.com>
2024-03-29 01:10:26 -0700

    footer added to tasks

--- react-vite/src/components/Tasks/TaskLandingPage.jsx
+++ react-vite/src/components/Tasks/TaskLandingPage.jsx
@@ -13,6 +13,7 @@ import NewToDoField from './NewToDoField';
 import NewRewardField from '../Rewards/NewRewardFiled';
 import { getRewards } from '../../redux/rewards';
 import RewardItemTile from '../Rewards/RewardItemTile';
+import Footer from '../Footer';
 // import EditTaskModal from '../EditTaskModal/EditTaskModal';
 // import EquipmentItem from "./EquipmentItem";
 
@@ -106,6 +107,14 @@ function TaskLandingPage() {
             ))}
         </div>
       </div>
+      <div className="waves_banner_three">
+        <div className="waves_three">
+          <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+            <path d="M-1.13,26.16 C156.32,123.85 358.92,-4.43 501.13,35.04 L500.00,0.00 L0.00,0.00 Z"></path>
+          </svg>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
