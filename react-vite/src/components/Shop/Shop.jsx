3f235c7fb4befddd613bb5c0e86ec79c87276b9a
Ramon <ramonpbarros@gmail.com>
2024-05-02 19:35:48 -0700

    shop page is now responsive

--- react-vite/src/components/Shop/Shop.jsx
+++ react-vite/src/components/Shop/Shop.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { getUserAvatar } from "../../redux/avatars";
+import { getUserAvatar } from '../../redux/avatars';
 import { getShopEquipmentThunk } from '../../redux/shop';
 import ViewAvatar from '../ViewAvatar/ViewAvatar';
 import ShopSection from './ShopSection';
@@ -10,10 +10,10 @@ import Footer from '../Footer';
 import './Shop.css';
 
 function Shop() {
-  const user = useSelector(state => state.session.user);
-  const avatar = useSelector(state => state.avatar.avatar);
+  const user = useSelector((state) => state.session.user);
+  const avatar = useSelector((state) => state.avatar.avatar);
   const dispatch = useDispatch();
-  const shopEquipment = useSelector(state => state.shop.equipment);
+  const shopEquipment = useSelector((state) => state.shop.equipment);
   const equipment = Object.values(shopEquipment);
 
   useEffect(() => {
@@ -21,13 +21,13 @@ function Shop() {
     dispatch(getShopEquipmentThunk());
   }, [user, avatar, dispatch]);
 
-  if (!user) return <Navigate to='/signup' replace={true} />;
+  if (!user) return <Navigate to="/signup" replace={true} />;
 
   return (
     <>
       <ViewAvatar />
-      <div className='shop-wrapper'>
-        <div className='equipment_heading'>Shop - Equipment</div>
+      <div className="shop-wrapper">
+        <div className="equipment_heading">Shop - Equipment</div>
         <div>
           <ShopSection equipment={equipment} />
         </div>
