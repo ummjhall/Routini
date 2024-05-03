2e3d9d13ab2199ed75fe1b504db49fb1d490e782
Ramon <ramonpbarros@gmail.com>
2024-05-02 20:01:43 -0700

    settings page responsive

--- react-vite/src/components/Settings/Settings.jsx
+++ react-vite/src/components/Settings/Settings.jsx
@@ -11,8 +11,8 @@ import Footer from '../Footer';
 import './Settings.css';
 
 function Settings() {
-  const user = useSelector(state => state.session.user);
-  const avatar = useSelector(state => state.avatar.avatar);
+  const user = useSelector((state) => state.session.user);
+  const avatar = useSelector((state) => state.avatar.avatar);
   const dispatch = useDispatch();
   const { setModalContent } = useModal();
 
@@ -22,66 +22,82 @@ function Settings() {
 
   const handleClickEdit = () => {
     setModalContent(<SettingsModal />);
-  }
+  };
 
   const handleClickFixValues = () => {
     setModalContent(<FixValuesModal />);
   };
 
-  if (!user) return <Navigate to='/signup' replace={true} />;
+  if (!user) return <Navigate to="/signup" replace={true} />;
 
   return (
     <>
       <ViewAvatar />
-      <div className='settings-wrapper'>
+      <div className="settings-wrapper">
         <h1>Settings</h1>
         <h2>Account</h2>
-        <div className='settings_account-wrapper'>
-          <div className='settings_account_row'>
-            <div className='settings_account_row-subcontainer'>
-              <div className='settings_account_row-start'>Email{' '}</div>
-              <div className='settings_account_row-middle'>{user.email}</div>
+        <div className="settings_account-wrapper">
+          <div className="settings_account_row">
+            <div className="settings_account_row-subcontainer">
+              <div className="settings_account_row-start">Email </div>
+              <div className="settings_account_row-middle">{user.email}</div>
             </div>
-            <div className='settings_account_edit-button' onClick={handleClickEdit}>
+            <div
+              className="settings_account_edit-button"
+              onClick={handleClickEdit}
+            >
               Edit
             </div>
           </div>
-          <div className='settings_account_row'>
-            <div className='settings_account_row-subcontainer'>
-              <div className='settings_account_row-start'>Username{' '}</div>
-              <div className='settings_account_row-middle'>{user.username}</div>
+          <div className="settings_account_row">
+            <div className="settings_account_row-subcontainer">
+              <div className="settings_account_row-start">Username </div>
+              <div className="settings_account_row-middle">{user.username}</div>
             </div>
-            <div className='settings_account_edit-button' onClick={handleClickEdit}>
+            <div
+              className="settings_account_edit-button"
+              onClick={handleClickEdit}
+            >
               Edit
             </div>
           </div>
-          <div className='settings_account_row'>
-            <div className='settings_account_row-subcontainer'>
-              <div className='settings_account_row-start'>Display name{' '}</div>
-              <div className='settings_account_row-middle'>{avatar?.name}</div>
+          <div className="settings_account_row">
+            <div className="settings_account_row-subcontainer">
+              <div className="settings_account_row-start">Display name </div>
+              <div className="settings_account_row-middle">{avatar?.name}</div>
             </div>
-            <div className='settings_account_edit-button' onClick={handleClickEdit}>
+            <div
+              className="settings_account_edit-button"
+              onClick={handleClickEdit}
+            >
               Edit
             </div>
           </div>
-          <div className='settings_account_row'>
-            <div className='settings_account_row-subcontainer'>
-              <div className='settings_account_row-start'>Password{' '}</div>
-              <div className='settings_account_row-middle'>{' '}</div>
+          <div className="settings_account_row">
+            <div className="settings_account_row-subcontainer">
+              <div className="settings_account_row-start">Password </div>
+              <div className="settings_account_row-middle">******** </div>
             </div>
-            <div className='settings_account_edit-button' onClick={handleClickEdit}>
+            <div
+              className="settings_account_edit-button"
+              onClick={handleClickEdit}
+            >
               Edit
             </div>
           </div>
         </div>
         <h2>Character</h2>
-        <div className='settings_account_row'>
-          <div className='settings_account_row-start'>Stats and Gold</div>
-          <div className='settings_account_edit-button' onClick={handleClickFixValues}>
+        <div className="settings_account_row">
+          <div className="settings_account_row-start">Stats and Gold</div>
+          <div
+            className="settings_account_edit-button"
+            onClick={handleClickFixValues}
+          >
             Fix values
           </div>
         </div>
       </div>
+      <br />
       <Wave />
       <Footer />
     </>
